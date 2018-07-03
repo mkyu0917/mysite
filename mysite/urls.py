5fc67e0b3e7a032c931b68b59ec36fb4d82227a6
minkyu <mkyu0917@gmail.com>
2018-07-04 01:55:50 +0900

    글쓰기 권한 완료

--- mysite/urls.py
+++ mysite/urls.py
@@ -54,6 +54,6 @@ urlpatterns = [ #url에서 post 방식으로 보내는 것들은 마지막에url
 
     #modify부분패턴
     path('board/modify', board_views.boardModify),
-    path('board/boardmodify_Save',board_views.boardModify_Save)
+    path('board/modify_Save',board_views.Modify_Save)
 
 ]
